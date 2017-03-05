51d70f852c67e503abf76598524dbe8f5eedb7cb
Aditya Purandare <aditya.p1993@hotmail.com>
2017-03-05 11:53:47 -0600

    Config gunicorn

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 import MySQLdb as mdb
 from config import *
+import os
 
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-con = mdb.connect(SQL_DATABASE_URI, SQL_DATABASE_USER, \
+con = mdb.connect(os.environ.get('SQL_DATABASE_URI'), SQL_DATABASE_USER, \
                   SQL_DATABASE_PASS, SQL_DATABASE_SCHEMA)
 
 from app import views
