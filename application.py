675ad4ce3baff98980126e67db970067bfdd73cb
Aditya Purandare <aditya.p1993@hotmail.com>
2017-03-29 19:18:38 -0500

    Fix unicorn heroku bug

--- application.py
+++ application.py
@@ -2,4 +2,6 @@ from app import app
 import os
 
 port = int(os.environ.get('PORT', 8000))
-app.run(host='0.0.0.0',port=port, debug=False)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=port, debug=debug_flag)
