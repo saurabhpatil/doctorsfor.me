b5481fa829a645bfe055247c9dc12d39de67bdec
Aditya Purandare <aditya.p1993@hotmail.com>
2017-03-30 21:45:19 -0500

    Add get reviews

--- app/routes.py
+++ app/routes.py
@@ -152,6 +152,7 @@ def delete_doctor(id):
 
     except Exception as e:
         con.rollback()
+        result['error'] = e 
         return json.dumps(result)
     finally:
         con.close()
@@ -165,7 +166,63 @@ def user_logout(user_type, id):
 
 @app.route('/review', methods=['GET'])
 def read_review():
-    pass
+    result = dict()
+    result['success'] = False
+
+    profile_id = request.args.get('id', None)
+    user_type = request.args.get('user_type', None)
+
+    # Check for null data
+    if profile_id is None or user_type is None:
+        result['error'] = 'Either profile_id or user_type is null.'
+        return result
+    elif user_type != 'doctor':
+        result['error'] = 'Request should contain doctor as user_type.'
+        return result
+
+    try:
+        # Connect to database
+        con = connect_database()
+        cursor = con.cursor()
+
+        # Get doctor_id from doctor table
+        sql_query = 'SELECT doctor_id FROM doctor WHERE profile_id={}'.format(profile_id)
+        cursor.execute(sql_query)
+        doctor_id = cursor.fetchone()
+        doctor_id = int(doctor_id[0])
+
+        # Get list of reviews from reviews table
+        sql_query = "SELECT R.review_id, R.score, R.comment, U.full_name \
+                     FROM reviews as R \
+                     INNER JOIN customer as C ON R.customer_id=C.customer_id \
+                     INNER JOIN user_profile as U ON C.profile_id=U.profile_id \
+                     WHERE R.doctor_id={}".format(doctor_id)
+        cursor.execute(sql_query)
+        reviews_iterator = cursor.fetchall()
+
+        result['reviews'] = list()
+
+        # Return list of reviews
+        for review in reviews_iterator:
+            review_dict = dict()
+            review_dict['review_id'] = int(review[0])
+            review_dict['score'] = int(review[1])
+            review_dict['comment'] = unicode(review[2])
+            review_dict['full_name'] = unicode(review[3])
+            result['reviews'].append(review_dict)
+        
+        # Close connections
+        cursor.close()
+        con.commit()
+        result['success'] = True
+        return json.dumps(result)
+
+    except Exception as e:
+        con.rollback()
+        result['error'] = str(e)
+        return json.dumps(result)
+    finally:
+        con.close()
 
 @app.route('/review', methods=['POST'])
 def create_review():
