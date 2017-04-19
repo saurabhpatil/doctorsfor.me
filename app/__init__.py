2b670b2cb15a5cd05ccba4a70a805f60451e1790
Aditya Purandare <aditya.p1993@hotmail.com>
2017-04-19 10:49:30 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 import MySQLdb as mdb
 from flask_cors import CORS
 
-
+# Create a flask app and configure it
 app = Flask(__name__)
 app.config.from_object('config')
+# Enable CORS 
 CORS(app)
 
 from app import routes
