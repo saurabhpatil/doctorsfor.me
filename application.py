27b3fa69d4132979ef327897b35d20c3cec265a5
Aditya Purandare <aditya.p1993@hotmail.com>
2017-04-19 10:50:17 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
 from app import app
 import os
 
-
+# Read PORT available on cloud server
 port = int(os.environ.get('PORT', 8000))
 
 if __name__ == '__main__':
+    # Run the flask app
     app.run(host='0.0.0.0',port=port, debug=debug_flag)
